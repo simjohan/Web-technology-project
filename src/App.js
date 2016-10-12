3d74baab6b4fd1a9421d4a7c777717aab114fd0e
Thomas Alm <thomaalm@stud.ntnu.no>
2016-10-12 13:13:30 +0200

    Created and styled index page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Title from "./components/app/Title"
+import Movie from "./components/app/Movie"
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+
+      <div className="newest-reviews">
+        <Title title="Nyeste Anmeldelser"/>
+        <ul className="movie-container">
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+        </ul>
+      </div>
+
+      <div className="newly-visited">
+        <Title title="Nylig Besøkte"/>
+        <ul className="movie-container">
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+        </ul>
+      </div>
+
       </div>
     );
   }
+
 }
 
 export default App;
