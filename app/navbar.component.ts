19e7355651f87219df841916f5a7c0f49d6e656a
Kakedragge <simonhst@stud.ntnu.no>
2016-10-15 19:51:00 +0200

    added comments to code

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component ({
+    //Selector "navigation-bar" lets other components use the template into their own template.
     selector: "navigation-bar",
+    //"nav-search" is a selector of another component.
     template:`
     <div id="navbar">
         <nav-search></nav-search>
