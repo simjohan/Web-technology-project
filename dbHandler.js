9791a9a34009e7c3ed86c7370546207e9699dbab
matstos <matstos@stud.ntnu.no>
2016-10-27 23:29:39 +0200

    The server is now able to get data from the client

--- dbHandler.js
+++ dbHandler.js
@@ -4,6 +4,7 @@ var file = "database.db";
 var exists = fs.existsSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
+var routes = require('routes.js');
 
 
 
