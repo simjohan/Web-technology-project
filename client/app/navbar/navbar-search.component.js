bfde6a17cb22c91baba539cc78f851e85e989cfb
Vi_H <vigdihan@stud.ntnu.no>
2016-11-06 19:53:11 +0100

    Removed navbar-search.component.css, since it does not do anything. Added color: "black" to the search field so you are able to see what you type

--- client/app/navbar/navbar-search.component.js
+++ client/app/navbar/navbar-search.component.js
@@ -30,7 +30,6 @@ var NavbarSearchComponent = (function () {
         core_1.Component({
             //Selector "nav-search" lets other components use the template into their own template.
             selector: "nav-search",
-            styleUrls: ["navbar-search.component.css"],
             template: "\n        <input class=\"navbar-search-field\" type=\"text\" #box placeholder=\"Search...\" (keyup.enter)=\"searchMovies()\" [(ngModel)] = \"str\" maxlength=\"25\"/>\n        <input class=\"btn navbar-search-submit-btn\" type=\"submit\" value=\"Submit\" (click)=\"searchMovies()\">\n    ",
         }), 
         __metadata('design:paramtypes', [router_1.Router])
