32c0a51d53b82fb6048a32ff8accbf86d6138f61
Vi_H <vigdihan@stud.ntnu.no>
2016-11-06 19:29:18 +0100

    If you enter the profile page, while logged in, you now get the correct information about you shown on the page

--- client/app/profile/profile-user-details.component.js
+++ client/app/profile/profile-user-details.component.js
@@ -9,22 +9,36 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var profile_service_1 = require('./profile.service');
 /**
  * @Component allows you to mark a class as an Angular component and provide additional metadata that determines
  * how the component should be processed, instantiated and used at runtime.
  */
 var ProfileUserDetailsComponent = (function () {
-    function ProfileUserDetailsComponent() {
+    function ProfileUserDetailsComponent(profileService, _ngZone) {
+        this.profileService = profileService;
+        this._ngZone = _ngZone;
         //Send in a user information into the view
-        this.userUsername = "Username";
-        this.userRealName = "Real Name";
         this.userDescription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
             "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" +
             "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
             "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        //Add a reviewTitle to the movie-review that is added in the movie-page.component.html
-        this.reviewTitle = "Good Movie";
     }
+    // On start of lifecycle
+    ProfileUserDetailsComponent.prototype.ngOnInit = function () {
+        var self = this;
+        //Use the facebook-api to get the ID from the user that is logged in
+        FB.api("/me", function (response) {
+            self._ngZone.run(function () {
+                self.getUserById(response.id);
+            });
+        });
+    };
+    // Use the profileService to get a specific user from the REST API based on id.
+    ProfileUserDetailsComponent.prototype.getUserById = function (userNameId) {
+        var _this = this;
+        this.profileService.getUser(userNameId).subscribe(function (data) { return _this.userById = data; }, function (error) { return console.log(error); });
+    };
     ProfileUserDetailsComponent = __decorate([
         core_1.Component({
             //moduleId makes it possible to use "templateUrl" - Angular 2 would look for the files at root level if we do not add this.
@@ -35,8 +49,10 @@ var ProfileUserDetailsComponent = (function () {
             templateUrl: 'profile-user-details.component.html',
             // stylrUlrs tells the component where it can find the CSS-code that it is going to use
             styleUrls: ['profile-user-details.component.css'],
+            // Provider tells the component which service to use
+            providers: [profile_service_1.ProfileService]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [profile_service_1.ProfileService, core_1.NgZone])
     ], ProfileUserDetailsComponent);
     return ProfileUserDetailsComponent;
 }());
--- client/app/profile/profile-user-details.component.ts
+++ client/app/profile/profile-user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import { ProfileService } from './profile.service';
 
 /**
  * @Component allows you to mark a class as an Angular component and provide additional metadata that determines
@@ -13,20 +14,39 @@ import { Component } from '@angular/core';
     templateUrl: 'profile-user-details.component.html',
     // stylrUlrs tells the component where it can find the CSS-code that it is going to use
     styleUrls: ['profile-user-details.component.css'],
+    // Provider tells the component which service to use
+    providers: [ProfileService]
 })
 
 /**
  * Exporting the class ProfilePageComponent, so other components have access to it
  */
-export class ProfileUserDetailsComponent {
+export class ProfileUserDetailsComponent  implements OnInit{
     //Send in a user information into the view
-    userUsername = "Username";
-    userRealName = "Real Name";
     userDescription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
         "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" +
         "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    //Add a reviewTitle to the movie-review that is added in the movie-page.component.html
-    reviewTitle = "Good Movie";
+
+    private userById: Object;
+
+    constructor (private profileService: ProfileService, private _ngZone: NgZone) {}
+
+    // On start of lifecycle
+    ngOnInit() {
+        let self = this;
+        //Use the facebook-api to get the ID from the user that is logged in
+        FB.api("/me", function(response){
+            self._ngZone.run(() => {
+                self.getUserById(response.id);
+            })
+        });
+    }
+
+    // Use the profileService to get a specific user from the REST API based on id.
+    getUserById(userNameId):void {
+        this.profileService.getUser(userNameId).subscribe(data => this.userById = data, error => console.log(error));
+
+    }
 }
