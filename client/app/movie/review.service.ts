fcceddd93ef1d48336ee7ada9165a1f08f79db56
matstos <matstos@stud.ntnu.no>
2016-11-08 13:07:34 +0100

    Removed test function. The deletion works

--- client/app/movie/review.service.ts
+++ client/app/movie/review.service.ts
@@ -49,9 +49,4 @@ export class ReviewService {
         return this.http.post(addReviewUrl, body, options).subscribe();
     }
 
-    // Test method to test the removal of compiled files
-    allJsRemovalTest(){
-        let test = "This is just a test";
-    }
-
 }
