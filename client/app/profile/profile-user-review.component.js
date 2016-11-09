3ea0fb8240c5bf6d992fdf3d6b50c89404af7a8e
Thomas Alm <thomaalm@stud.ntnu.no>
2016-11-09 13:14:02 +0100

    Reverted some changes, styled some stuff.

---
+++ client/app/profile/profile-user-review.component.js
@@ -0,0 +1,55 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+/**
+ * @Component allows you to mark a class as an Angular component and provide additional metadata that determines
+ * how the component should be processed, instantiated and used at runtime.
+ */
+var ProfileUserReview = (function () {
+    function ProfileUserReview() {
+        this.userReviewMovieTitle = "MOVIE TITLE";
+        this.userReviewTitle = "REVIEW TITLE";
+        this.userReviewRating = "REVIEW RATING";
+        this.userReviewText = "REVIEW TEXT";
+    }
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], ProfileUserReview.prototype, "userReviewMovieTitle", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], ProfileUserReview.prototype, "userReviewTitle", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], ProfileUserReview.prototype, "userReviewRating", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], ProfileUserReview.prototype, "userReviewText", void 0);
+    ProfileUserReview = __decorate([
+        core_1.Component({
+            //moduleId makes it possible to use "templateUrl" - Angular 2 would look for the files at root level if we do not add this.
+            moduleId: module.id,
+            // Selector "profile-user-review" lets other components use the template into their own template
+            selector: "profile-user-review",
+            //TemplateUrl tells the component where it can find the HTML-code it is going to show
+            templateUrl: 'profile-user-review.component.html',
+            // stylrUlrs tells the component where it can find the CSS-code that it is going to use
+            styleUrls: ['profile-user-review.component.css'],
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ProfileUserReview);
+    return ProfileUserReview;
+}());
+exports.ProfileUserReview = ProfileUserReview;
+//# sourceMappingURL=profile-user-review.component.js.map
